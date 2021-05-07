a78c93ffad3be5fe8c59dfeb9356f8e4aebad25a
Dariusz Adamczyk <dariusz.adamczyk@oktawave.com>
2021-05-07 11:18:01 +0200

    fix

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
-variable "oktawave_access_token" {
-  description = "OKTAWAVE access token"
-  type        = string
-}
-
 provider "oktawave" {
-  api_url      = "https://api.oktawave.com/beta"
+  api_url      = "https://pl1-api.oktawave.com/services"
+  # endpoint for PL-2-KRK https://pl2-api.oktawave.com/services
   access_token = var.oktawave_access_token
 }
